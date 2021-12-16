171f62791d395a1065a8486f9e7ec2c5f6efea41
Jordan Borean <jborean93@gmail.com>
2021-12-16 21:54:55 +1000

    Start to add get parameters

--- src/Commands/OpenADObject.cs
+++ src/Commands/OpenADObject.cs
@@ -3,11 +3,55 @@ using System.Management.Automation;
 
 namespace PSOpenAD.Commands
 {
+    public enum SearchScope
+    {
+        Base,
+        OneLevel,
+        Subtree,
+    }
+
     [Cmdlet(
-        VerbsCommon.Get, "OpenADObject"
+        VerbsCommon.Get, "OpenADObject",
+        DefaultParameterSetName = "Identity"
     )]
     public class GetOpenADObject : PSCmdlet
     {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "Identity"
+        )]
+        public string Identity { get; set; } = "";
+
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "LDAPFilter"
+        )]
+        public string LDAPFilter { get; set; } = "";
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public OpenADSession? Session { get; set; }
+
+        [Parameter()]
+        public string[] Property { get; set; } = Array.Empty<string>();
+
+        [Parameter()]
+        public string SearchBase { get; set; } = "";
+
+        [Parameter()]
+        public SearchScope SearchScope { get; set; } = SearchScope.Subtree;
+
+
+        [Parameter()]
+        public SwitchParameter IncludeDeletedObjects { get; set; }
+
         protected override void ProcessRecord()
         {
             WriteObject("Test");
