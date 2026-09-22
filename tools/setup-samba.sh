4b855d9267ddbf2dd32ce332877b6786143d560f
Jordan Borean <jborean93@gmail.com>
2026-09-23 04:39:33 +1000

    Try to quiet the apt-get install and update logs

--- tools/setup-samba.sh
+++ tools/setup-samba.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q -o Dpkg::Use-Pty=0 \
     acl \
     attr \
     dnsutils \
